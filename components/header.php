75b4a6ddb242d224bf3439b3b9018d19de6d9ec7
David Santos <david.tecds@gmail.com>
2025-04-21 22:31:16 -0300

    Creating header

---
+++ components/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wave</title>
+</head>
+<body>
+
+    <header>
+        <h1>Wave</h1>
+        <nav>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="sobre.php">Sobre</a></li>
+                <li><a href="contato.php">Contato</a></li>
+            </ul>
+        </nav>
+    </header>
+    
+</body>
+</html>
