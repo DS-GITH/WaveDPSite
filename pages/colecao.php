f3ded80a4d9fa2fa77c70beee8e78ab00e1bf229
Luizamc08 <david.tecds@gmail.com>
2025-05-05 10:44:39 -0300

    Almost done, just do the design better

--- pages/colecao.php
+++ pages/colecao.php
@@ -1,14 +1,12 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="./colecao.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Coleções</title>
+  <link rel="stylesheet" href="./colecao.css">
 </head>
 <body>
-
-
 
 <?php include_once '../includes/Header/header.php'; ?>
 
@@ -16,47 +14,57 @@
 
 <?php 
 
-$colections =[
-    [
-        "nome" => "Primeira",
-        "descricao" => "A primeira coleção de blusas.",
-        "imagem" => "../assets/wavecolection.png",
-        "textabout" => "Essa é a primeira coleção de blusas, com estampas exclusivas e cores vibrantes. 
-        Cada peça foi cuidadosamente desenhada para oferecer conforto e estilo. Venha conferir!",
-    ],
-
+$colections = [
+  [
+    "nome" => "Primeira",
+    "imagem" => "../assets/wavecolection.png",
+    "textabout" => "Essa é a primeira coleção de blusas, com estampas exclusivas e cores vibrantes. Cada peça foi cuidadosamente desenhada para oferecer conforto e estilo. Venha conferir!",
+  ],
+  [
+    "nome" => "Segunda",
+    "imagem" => "../assets/wavecolection.png",
+    "textabout" => "Essa é a segunda coleção de blusas, com estampas exclusivas e cores vibrantes. Cada peça foi cuidadosamente desenhada para oferecer conforto e estilo. Venha conferir!",
+  ],
+  [
+    "nome" => "Terceira",
+    "imagem" => "../assets/wavecolection.png",
+    "textabout" => "Essa é a terceira coleção de blusas, com estampas exclusivas e cores vibrantes. Cada peça foi cuidadosamente desenhada para oferecer conforto e estilo. Venha conferir!",
+  ],
 ];
 
-
-if (empty($colections)){
-    echo "<h2> Nenhuma coleção encontrada. </h2>";
+// Exibe as coleções normalmente com links
+if (empty($colections)) {
+  echo "<h2>Nenhuma coleção encontrada.</h2>";
 } else {
-    foreach ($colections as $colecao){
-        echo "<a href='#'>";
-        echo "<div class='colecao'>";
-        echo "<img src='" . $colecao['imagem'] . "' alt='" . $colecao['nome'] . "'>";
-        echo "<h3 class='name'>" . $colecao['nome'] . "</h3>";
-        echo "<p class='desc'>" . $colecao['descricao'] . "</p>";
-        echo "</div>";
-        echo "</a>";
-    }
-}
-
-foreach ($colections as $colecao){
-    echo "<div class='hwindow'>";
-    echo "<h2 class='hname'>" . $colecao['nome'] . "</h2>";
+  foreach ($colections as $colecao) {
+    echo "<a href='?colecao=" . urlencode($colecao['nome']) . "'>";
+    echo "<div class='colecao'>";
     echo "<img src='" . $colecao['imagem'] . "' alt='" . $colecao['nome'] . "'>";
-
-    echo "<p class='about'>" . $colecao['textabout'] . "</p>";
+    echo "<h3 class='name'>" . $colecao['nome'] . "</h3>";
+    echo "</div>";
+    echo "</a>";
+  }
 }
 
+// Exibe a janela somente se for clicada
+if (isset($_GET['colecao'])) {
+  $colecaoSelecionada = $_GET['colecao'];
+
+  foreach ($colections as $colecao) {
+    if ($colecao['nome'] === $colecaoSelecionada) {
+      echo "<div class='hwindow'>";
+      echo "<a href='colecao.php' class='close'>&times;</a>";
+      echo "<h2 class='hname'>" . $colecao['nome'] . "</h2>";
+      echo "<img src='" . $colecao['imagem'] . "' alt='" . $colecao['nome'] . "'>";
+      echo "<p class='about'>" . $colecao['textabout'] . "</p>";
+      echo "</div>";
+    }
+  }
+}
 ?>
 
 </div>
 
-
-<?php include_once '../includes/Footer/footer.php' ?>
-
-    
+<?php include_once '../includes/Footer/footer.php'; ?>
 </body>
 </html>
