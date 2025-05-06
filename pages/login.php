b19fe0a0db0843f454efa845d2f8ad6bb13df8b3
Luizamc08 <david.tecds@gmail.com>
2025-05-06 14:43:40 -0300

    Added return button
    button to return to the main page

--- pages/login.php
+++ pages/login.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./login.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" />
     <title>Document</title>
 </head>
 <body>
@@ -11,6 +12,8 @@
 
 
 <main class="container">
+
+    <p><a href="./index.php" class="back"> <i class="fa-solid fa-arrow-left"></i> Voltar para a loja </a></p>
 
     <div class="logoBox">
         <img src="../assets/Wave.png" alt="Logo" class="logo">
